fe2957c4a3c2c65d454b56816e3553d331a8315c
Bende0724 <168254118+Glitchederror0724@users.noreply.github.com>
2026-08-15 19:32:20 -0400

    Update free_surface.lua with Noclip feature
    Added Noclip feature to the Delta Ultimate v5.1 script.

--- free_surface.lua
+++ free_surface.lua
@@ -2,7 +2,7 @@
 -- 👇 YOUR MAIN SCRIPT CODE STARTS HERE 👇
 -------------------------------------------------------------------------------
 
-
+--
 --Here's the complete Delta Ultimate v5.1 script with Noclip added:
 
 -- Services
